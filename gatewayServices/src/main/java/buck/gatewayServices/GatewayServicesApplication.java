bf7dcafd1471cfa7096707e84b8483224f62fd71
kuangbuckk <kuangbuckk159@gmail.com>
2025-08-08 15:59:44 +0700

    add resilient features: circuit breaker and redis ratelimiter

--- gatewayServices/src/main/java/buck/gatewayServices/GatewayServicesApplication.java
+++ gatewayServices/src/main/java/buck/gatewayServices/GatewayServicesApplication.java
@@ -3,9 +3,12 @@ package buck.gatewayServices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
@@ -22,7 +25,11 @@ public class GatewayServicesApplication {
 		return routeLocatorBuilder.routes()
 				.route(p -> p.path("/buck/accounts/**")
 						.filters(f -> f.rewritePath("/buck/accounts/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver()))
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+										.setFallbackUri("forward:/contactSupport")))
 //						lb: Load balancer, tell gateway to do client side load balancer with the help of
 //						spring cloud balancer
 						.uri("lb://ACCOUNT-SERVICES"))
@@ -32,8 +39,20 @@ public class GatewayServicesApplication {
 						.uri("lb://STATISTIC-SERVICES"))
 				.route(p -> p.path("/buck/notification/**")
 						.filters(f -> f.rewritePath("/buck/notification/(?<segment>.*)", "/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+										.setKeyResolver(userKeyResolver())))
 						.uri("lb://NOTIFICATION-SERVICES"))
 				.build();
+	}
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter(){
+		return new RedisRateLimiter(1, 1, 1);
+	}
+
+	@Bean
+	KeyResolver userKeyResolver() {
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getQueryParams().getFirst("user")).defaultIfEmpty("anonymous");
 	}
 }
---
+++ gatewayServices/src/main/java/buck/gatewayServices/controller/FallbackController.java
@@ -0,0 +1,13 @@
+package buck.gatewayServices.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+    @RequestMapping("/contactSupport")
+    public Mono<String> contactSupport(){
+        return Mono.just("An error occured. Please try after contacting support team.");
+    }
+}
